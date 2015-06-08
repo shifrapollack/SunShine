a3b44c49ce5c9707f35aa02c9e338b7b07cf21b1
shifrapollack <shifrapollack@gmail.com>
2015-06-08 18:56:52 -0400

    Initial commit

---
+++ app/src/main/java/app/com/example/shifra/sunshine/MainActivity.java
@@ -0,0 +1,15 @@
+package app.com.example.shifra.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
